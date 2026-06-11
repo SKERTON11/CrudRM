e7eb839104534fcbccbe831c375047d603872b51
SKERTON11 <stoknoora@gmail.com>
2026-06-12 01:01:14 +0700

    Update db.php
    ganti database

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$koneksi = mysqli_connect("localhost", "root", "", "db_dpcrud");
+$koneksi = mysqli_connect("localhost", "root", "", "randumekar");
 
 if (!$koneksi) {
     die("Koneksi gagal: " . mysqli_connect_error());
